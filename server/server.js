7ca26e0303edbe6ff1b441fc9f65953827b52d0c
Viraj Jadhav <71481611+Virajj28@users.noreply.github.com>
2025-02-09 03:12:28 +0000

    push ping conn

--- server/server.js
+++ server/server.js
@@ -2,15 +2,40 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// mongoose.connect(process.env.MONGO_URI)
+// mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 //     .then(() => console.log("MongoDB Connected"))
 //     .catch(err => console.log(err));
+
+const uri = "mongodb+srv://user0:user0@cluster0.w22uk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => res.send("Server is running"));
 
